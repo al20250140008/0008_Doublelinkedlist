e4b34257419ba40a16e21f275f5ee84c1d896f12
al20250140008 <alf80169@gmail.com>
2026-04-30 20:14:45 +0700

    insert tengah dan akhir

--- 0008_Doublelinkedlist.cpp
+++ 0008_Doublelinkedlist.cpp
@@ -64,4 +64,14 @@ public:
         }
 
         
-       
+        newNode->next = current->next; 
+        newNode->prev = current;       
+
+        
+        if (current->next != NULL) 
+            current->next->prev = newNode; 
+
+        current->next = newNode; 
+    }
+
+    
